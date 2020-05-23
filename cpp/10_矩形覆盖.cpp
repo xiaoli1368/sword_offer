c4815fa773886e120a672b3075e1b72407235420
xiaoli1368 <xiaoli1644@qq.com>
2020-05-23 09:50:09 +0800

    modified the 10th answer

--- cpp/10_矩形覆盖.cpp
+++ cpp/10_矩形覆盖.cpp
@@ -1,1 +1,41 @@
-// 见变态跳台阶
+#include <iostream>
+#include <stdio.h>
+#include <sys/time.h>
+
+class Solution {
+public:
+    int jumpFloot(int n) {
+        if (n < 3) {
+            return n;
+        }
+
+        int a = 1, b = 2, c = 3;
+        for (int i = 0; i < n - 2; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    // 测试函数
+    void test(int n) {
+        int result = 0;
+        struct timeval start, end;
+
+        gettimeofday(&start, nullptr);
+        result = this->jumpFloot(n);
+        gettimeofday(&end, nullptr);
+        printf("time(us): %ld, input: %2d, result: %d\n", end.tv_usec - start.tv_usec, n, result);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    for (int i = 0; i <= 40; i += 10) {
+        s.test(i);
+    }
+
+    return 0;
+}
--- python/10_矩形覆盖.py
+++ python/10_矩形覆盖.py
@@ -1,1 +1,37 @@
-# 见变态跳台阶
+#!/bin/bash python3
+#-*- coding:utf-8 -*-
+
+import time
+
+class Solution():
+    def jumpFloor(self, n):
+        """
+        动态规划
+        """
+        if n < 3:
+            return n
+
+        a, b, c = 1, 2, 3
+        for i in range(n - 2):
+            c = a + b
+            a, b = b, c
+        return c
+
+    def test(self, n):
+        """
+        测试函数
+        """
+        start = time.time()
+        result = self.jumpFloor(n)
+        end = time.time()
+        print("time(us): {:5.2f}, input: {:2d}, result: {:d}".format((end - start)*10**6, n, result))
+
+
+def main():
+    s = Solution()
+    for i in range(0, 41, 10):
+        s.test(i)
+
+
+if __name__ == "__main__":
+    main()
